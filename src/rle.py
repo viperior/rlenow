18918018021fe60f8d0d99c56470dd7de876c399
Johnathon Stone <viperior@gmail.com>
2023-01-20 02:53:24 +0000

    Add an optional limit parameter for run length

--- src/rle.py
+++ src/rle.py
@@ -1,7 +1,7 @@
 """Run-length encoding algorithm implementation"""
 
 
-def encode(data: str) -> str:
+def encode(data: str, limit: int = 9) -> str:
     """Return the input data encoded using Run-length encoding (RLE)"""
     encoded = ''
     n = len(data)
@@ -10,7 +10,7 @@ def encode(data: str) -> str:
         return encoded
     while i < n:
         count = 1
-        while i < n - 1 and data[i] == data[i+1]:
+        while i < n - 1 and data[i] == data[i+1] and count < limit:
             count += 1
             i += 1
         i += 1
