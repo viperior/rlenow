9d2a1700b67ec1d6481f68c4497dcc31add8cfe8
Johnathon Stone <viperior@gmail.com>
2023-01-20 02:39:55 +0000

    Define test cases for RLE compression

---
+++ tests/test_rle_encode.py
@@ -0,0 +1,30 @@
+"""Tests for the rle_encode() function"""
+
+import pytest
+
+import src.rle as rle
+
+
+@pytest.mark.parametrize(
+    'test_input,expected',
+    [
+        ('a', '1a'),
+        ('aa', '2a'),
+        ('1', '11'),
+        ('11', '21'),
+        ('111', '31'),
+        ('wwwawwwawwwa', '3w1a3w1a3w1a'),
+        ('wwwwwwwwwwawwwwwwwwwwawwwwwwwwwwa', '10w1a10w1a10w1a'),
+        ('', ''),
+        ('aaab', '3a1b'),
+    ]
+)
+def test_rle_encode(test_input: str, expected):
+    """Tests for the rle_encode() function"""
+    assert rle.encode(test_input) == expected
+
+
+# def test_rle_encode_empty_input():
+#     """Test the RLE encoding function using an empty string as input"""
+#     with pytest.raises(ValueError):
+#         rle.encode('')
