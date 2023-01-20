a77bb8b7ba99d6983c274d4755f4ae400469dfc8
Johnathon Stone <viperior@gmail.com>
2023-01-20 02:40:04 +0000

    Implement RLE compression algorithm

---
+++ src/rle.py
@@ -0,0 +1,19 @@
+"""Run-length encoding algorithm implementation"""
+
+
+def encode(data: str) -> str:
+    """Return the input data encoded using Run-length encoding (RLE)"""
+    encoded = ''
+    n = len(data)
+    i = 0
+    if n == 0:  # Handle empty string input by returning an empty string
+        return encoded
+    while i < n:
+        count = 1
+        while i < n - 1 and data[i] == data[i+1]:
+            count += 1
+            i += 1
+        i += 1
+        encoded += str(count)
+        encoded += data[i-1]
+    return encoded
